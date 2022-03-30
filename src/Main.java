9a6522d7df305b4c9c6d73bc50c2cf6f917b975b
Петр Петров <3033106@gmail.com>
2022-03-30 20:33:02 +0400

    commit installationProject

--- src/Main.java
+++ src/Main.java
@@ -8,48 +8,48 @@ public class Main {
         StringBuilder logo = new StringBuilder();
 
 
-        File src = new File("C:/Study/Games/src");
+        File src = new File("Games/src");
         if (src.mkdir())logo.append("Создана директория " + src.getName() + "\n");
 
-        File res = new File("C:/Study/Games/res");
+        File res = new File("Games/res");
         if (res.mkdir())logo.append("Создана директория " + res.getName() + "\n");
 
-        File savegames = new File("C:/Study/Games/savegames");
+        File savegames = new File("Games/savegames");
         if (savegames.mkdir())logo.append("Создана директория " + savegames.getName() + "\n");
 
-        File temp = new File("C:/Study/Games/temp");
+        File temp = new File("Games/temp");
         if (temp.mkdir())logo.append("Создана директория " + temp.getName() + "\n");
 
-        File main = new File("C:/Study/Games/src/main");
+        File main = new File("Games/src/main");
         if (main.mkdir())logo.append("Создана директория " + main.getName() + "\n");
 
-        File test = new File("C:/Study/Games/src/test");
+        File test = new File("Games/src/test");
         if (test.mkdir())logo.append("Создана директория " + test.getName() + "\n");
 
-        File mainJava = new File("C:/Study/Games/src/main/Main.java");
+        File mainJava = new File("Games/src/main/Main.java");
         try {
             if (mainJava.createNewFile()) logo.append("Создан файл " + mainJava.getName() + "\n");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        File utilsJava = new File("C:/Study/Games/src/main/Utils.java");
+        File utilsJava = new File("Games/src/main/Utils.java");
         try {
             if (utilsJava.createNewFile()) logo.append("Создан файл " + utilsJava.getName() + "\n");
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
 
-        File drawables = new File("C:/Study/Games/res/drawables");
+        File drawables = new File("Games/res/drawables");
         if (drawables.mkdir())logo.append("Создана директория " + drawables.getName() + "\n");
 
-        File vectors = new File("C:/Study/Games/res/vectors");
+        File vectors = new File("Games/res/vectors");
         if (vectors.mkdir())logo.append("Создана директория " + vectors.getName() + "\n");
 
-        File icons = new File("C:/Study/Games/res/icons");
+        File icons = new File("Games/res/icons");
         if (icons.mkdir())logo.append("Создана директория " + icons.getName() + "\n");
 
-        File tempJava = new File("C:/Study/Games/temp/temp.txt");
+        File tempJava = new File("Games/temp/temp.txt");
         try {
             if (tempJava.createNewFile()) logo.append("Создан файл " + tempJava.getName() + "\n");
         } catch (IOException e) {
